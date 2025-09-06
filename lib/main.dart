92ac80fb7e14b1571e1bdac7077c5aea2653f99a
Threshika <threshikavijay@gmail.com>
2025-09-06 14:56:28 +0530

    Game page UI setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,63 +6,105 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Main Page")),
-        body: Center(
-          child: MaterialButton(
-            height: 50,
-            color: Colors.amber,
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SecondApp("Hello World","Holla")),
-              );
-            },
-            child: Text("Click me"),
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            color: Colors.lightBlueAccent,
+            height: MediaQuery.of(context).size.height / 2,
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: MaterialButton(
+                color: Colors.white,
+                shape: CircleBorder(),
+                height: 150,
+                minWidth: 150,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GamePage()),
+                  );
+                },
+                child: Text("Start"),
+              ),
+            ),
           ),
-        ),
+          Container(
+            color: Colors.redAccent,
+
+            height: MediaQuery.of(context).size.height / 2,
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: MaterialButton(
+                color: Colors.white,
+                shape: CircleBorder(),
+                height: 150,
+                minWidth: 150,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GamePage()),
+                  );
+                },
+                child: Text("Start"),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
-class SecondApp extends StatefulWidget {
-  String label1="";
-  String label2="";
-  //constructor
-  SecondApp(this.label1,this.label2);
-
-  @override
-  State<SecondApp> createState() => _SecondAppState();
-}
-
-class _SecondAppState extends State<SecondApp> {
+class GamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.amber, title: Text("Second page")),
-      body: Center(
-        child: Column(
+      body: Column(
         children: [
-          Text(widget.label1),
-          Text(widget.label2),
-
-          MaterialButton(
-            color: Colors.amber,
-            child: Text("Go Back (POP)"),
-            onPressed: (){
-              Navigator.pop(context);
-          }
-          
-          )
+          Container(
+            color: Colors.lightBlueAccent,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2,
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.all(15),
+            child: Row(
+              children: [
+                Expanded(child: Text("Player A",style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20
+                ),)),
+                Text("10",style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),)
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.redAccent,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2,
+            padding: EdgeInsets.all(15),
+            alignment: Alignment.bottomLeft,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text("Player B",style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20
+                ),),
+                ),
+                
+                Text("5",style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),),
+              ],
+            ),
+          ),
         ],
       ),
-      )
     );
   }
 }
